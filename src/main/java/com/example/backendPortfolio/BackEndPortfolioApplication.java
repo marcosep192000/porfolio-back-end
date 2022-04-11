69141634a3da6e580859641e1026c8c305fe811c
Marcos <69874820+marcosep192000@users.noreply.github.com>
2022-04-10 22:18:18 -0300

    primer commit

---
+++ src/main/java/com/example/backendPortfolio/BackEndPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendPortfolio/BackEndPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
