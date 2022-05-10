4797ae1a762d922ce609718cb87d431186b9566e
marcos <marcosep192000@hotmail.com>
2022-05-10 00:25:02 -0300

    correccion entity idioma

--- src/main/java/com/example/backendPortfolio/entity/Idioma.java
+++ src/main/java/com/example/backendPortfolio/entity/Idioma.java
@@ -17,4 +17,14 @@ public class Idioma {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @Column(name = "idioma", nullable = false)
+    private String idioma;
+    @Column(name = "nivel", nullable = false)
+        private String nivel;
+    @Column(name = "nivel_escrito", nullable = false)
+        private String nivelEscrito;
+    @Column(name = "nivel_oral", nullable = false)
+        private String nivelOral;
+
+
 }
